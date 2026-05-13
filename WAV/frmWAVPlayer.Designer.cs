3f67acab81f0da660c1c2d212162fcdc42eaa462
zzdonkey0327 <exonina2018027@gmail.com>
2026-05-13 21:33:14 +0800

    加入專案檔案。

---
+++ WAV/frmWAVPlayer.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WAV
+{
+    partial class frmWAVPlayer
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnLoop;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.OpenFileDialog ofdWAVFile;
+        private System.Windows.Forms.Label lblPath;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnLoop = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.ofdWAVFile = new System.Windows.Forms.OpenFileDialog();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Font = new System.Drawing.Font("微軟正黑體", 10F, System.Drawing.FontStyle.Bold);
+            this.lblPath.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.lblPath.Location = new System.Drawing.Point(36, 18);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(95, 22);
+            this.lblPath.TabIndex = 6;
+            this.lblPath.Text = "檔案路徑：";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Font = new System.Drawing.Font("微軟正黑體", 10F);
+            this.txtPath.Location = new System.Drawing.Point(40, 48);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(420, 30);
+            this.txtPath.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnBrowse.FlatAppearance.BorderSize = 0;
+            this.btnBrowse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBrowse.Font = new System.Drawing.Font("微軟正黑體", 10F, System.Drawing.FontStyle.Bold);
+            this.btnBrowse.ForeColor = System.Drawing.Color.White;
+            this.btnBrowse.Location = new System.Drawing.Point(480, 46);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(100, 34);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "選擇檔案";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnPlay.FlatAppearance.BorderSize = 0;
+            this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlay.Font = new System.Drawing.Font("微軟正黑體", 10F, System.Drawing.FontStyle.Bold);
+            this.btnPlay.ForeColor = System.Drawing.Color.White;
+            this.btnPlay.Location = new System.Drawing.Point(40, 110);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(110, 40);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "▶ 播放一次";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnLoop
+            // 
+            this.btnLoop.BackColor = System.Drawing.Color.Orange;
+            this.btnLoop.FlatAppearance.BorderSize = 0;
+            this.btnLoop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoop.Font = new System.Drawing.Font("微軟正黑體", 10F, System.Drawing.FontStyle.Bold);
+            this.btnLoop.ForeColor = System.Drawing.Color.White;
+            this.btnLoop.Location = new System.Drawing.Point(170, 110);
+            this.btnLoop.Name = "btnLoop";
+            this.btnLoop.Size = new System.Drawing.Size(110, 40);
+            this.btnLoop.TabIndex = 3;
+            this.btnLoop.Text = "🔁 重複播放";
+            this.btnLoop.UseVisualStyleBackColor = false;
+            this.btnLoop.Click += new System.EventHandler(this.btnLoop_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.IndianRed;
+            this.btnStop.FlatAppearance.BorderSize = 0;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStop.Font = new System.Drawing.Font("微軟正黑體", 10F, System.Drawing.FontStyle.Bold);
+            this.btnStop.ForeColor = System.Drawing.Color.White;
+            this.btnStop.Location = new System.Drawing.Point(300, 110);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(110, 40);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "⏹ 停止播放";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.BackColor = System.Drawing.Color.Gray;
+            this.btnEnd.FlatAppearance.BorderSize = 0;
+            this.btnEnd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEnd.Font = new System.Drawing.Font("微軟正黑體", 10F, System.Drawing.FontStyle.Bold);
+            this.btnEnd.ForeColor = System.Drawing.Color.White;
+            this.btnEnd.Location = new System.Drawing.Point(470, 110);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(110, 40);
+            this.btnEnd.TabIndex = 5;
+            this.btnEnd.Text = "✖ 結束程式";
+            this.btnEnd.UseVisualStyleBackColor = false;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // frmWAVPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(620, 190);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnLoop);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "frmWAVPlayer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WAV 音效檔播放器";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmWAVPlayer_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ WAV/frmWAVPlayer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Media;
+using System.Windows.Forms;
+
+namespace WAV
+{
+    public partial class frmWAVPlayer : Form
+    {
+        private SoundPlayer player = new SoundPlayer();
+
+        public frmWAVPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            ofdWAVFile.Filter = "WAV Files (*.wav)|*.wav";
+            ofdWAVFile.FileName = "";
+
+            if (ofdWAVFile.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = ofdWAVFile.FileName;
+            }
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtPath.Text))
+            {
+                MessageBox.Show("請先選擇 WAV 檔案！");
+                return;
+            }
+
+            try
+            {
+                player.SoundLocation = txtPath.Text;
+                player.Load();
+                player.Play();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("播放失敗：" + ex.Message);
+            }
+        }
+
+        private void btnLoop_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtPath.Text))
+            {
+                MessageBox.Show("請先選擇 WAV 檔案！");
+                return;
+            }
+
+            try
+            {
+                player.SoundLocation = txtPath.Text;
+                player.Load();
+                player.PlayLooping();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("重複播放失敗：" + ex.Message);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void frmWAVPlayer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var result = MessageBox.Show(
+                "確定要關閉應用程式嗎？",
+                "關閉確認",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
